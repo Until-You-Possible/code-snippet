c59408ce849465f1f8ced21e399f0b57931d2514
Gang Wang <gang.wang05@sap.com>
2021-08-23 16:05:19 +0800

    修改部分函数

--- javascript/string.js
+++ javascript/string.js
@@ -1,21 +1,19 @@
 
 
- string = {
-     camelize: function (sString) {
-         const rCamelCase = /-(.)/ig;
-         return sString.replace(rCamelCase, function (sMatch, sChar) {
-             return sChar.toUpperCase();
-         }); // // camelize("foo-bar"); // fooBar
-     },
-     // @param {string} sString String for which first character should be converted
-     capitalize:function (sString) {
-         return sString.charAt(0).toUpperCase() + sString.substring(1);
-     },
-     // sString camel case string
-     hyphenate: function (sString) {
-         const rHyphen = /([A-Z])/g;
-         return sString.replace( rHyphen, function(sMatch, sChar) {
-             return "-" + sChar.toLowerCase();
-         });
-     }
- }
+function camelize(sString) {
+    const rCamelCase = /-(.)/ig;
+    return sString.replace(rCamelCase, function (sMatch, sChar) {
+        return sChar.toUpperCase();
+    }); // // camelize("foo-bar"); // fooBar
+}
+
+function capitalize(sString) {
+    return sString.charAt(0).toUpperCase() + sString.substring(1);
+}
+
+function hyphenate(sString) {
+    const rHyphen = /([A-Z])/g;
+    return sString.replace( rHyphen, function(sMatch, sChar) {
+        return "-" + sChar.toLowerCase();
+    });
+}
